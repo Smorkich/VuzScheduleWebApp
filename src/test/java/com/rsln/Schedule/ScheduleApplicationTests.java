e37b21595fc51c5d1516ede410237b74c848759e
Каракян Руслан <ruslan01.03@mail.ru>
2025-11-23 15:59:16 +0300

    init

---
+++ src/test/java/com/rsln/Schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsln.Schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
