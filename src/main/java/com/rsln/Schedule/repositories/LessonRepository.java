9e4103711a61270b27b4049fdd4824fdae49f378
Каракян Руслан <ruslan01.03@mail.ru>
2025-12-30 16:07:52 +0300

    add EntityGraph fix N+1

--- src/main/java/com/rsln/Schedule/repositories/LessonRepository.java
+++ src/main/java/com/rsln/Schedule/repositories/LessonRepository.java
@@ -1,6 +1,7 @@
 package com.rsln.Schedule.repositories;
 
 import com.rsln.Schedule.models.Lesson;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.DayOfWeek;
@@ -9,13 +10,18 @@ import java.util.Optional;
 
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
 
+    @EntityGraph(attributePaths = {"group", "subject", "teacher", "classroom"})
     List<Lesson> findByGroupId(Long groupId);
 
+    @EntityGraph(attributePaths = {"group", "subject", "teacher", "classroom"})
     List<Lesson> findByTeacherId(Long teacherId);
 
+    @EntityGraph(attributePaths = {"group", "subject", "teacher", "classroom"})
     List<Lesson> findByTeacherIdAndDayOfWeek(Long teacherId, DayOfWeek day);
 
+    @EntityGraph(attributePaths = {"group", "subject", "teacher", "classroom"})
     List<Lesson> findByDayOfWeek(DayOfWeek dayOfWeek);
 
+    @EntityGraph(attributePaths = {"group", "subject", "teacher", "classroom"})
     List<Lesson> findByGroupIdAndDayOfWeek(Long groupId, DayOfWeek dayOfWeek);
 }
